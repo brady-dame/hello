7f15fe2cbe6eadae8fde26e2f14417a82a9265a4
brady-dame <bdame2@huskers.unl.edu>
2025-08-26 20:02:22 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/*
+* Author: Brady Dame
+*Created On: 2025-08-26
+*
+*Prints my name to the console
+*/
+#include <stlib.h>
+#include <stdio.h>
+
+int main (argc, char **argv){
+
+    printf("My names is Brady Dame");
+
+    return 0;
+}
